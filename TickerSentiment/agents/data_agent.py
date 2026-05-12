c432aab5138c9d7f6444819f941c6336b5ce34dc
Kyriakos <kyriakos.kyriakatos@gmail.com>
2026-05-12 18:13:05 +0200

    modified:   TickerSentiment/agents/__pycache__/data_agent.cpython-312.pyc 	modified:   TickerSentiment/agents/data_agent.py

--- TickerSentiment/agents/data_agent.py
+++ TickerSentiment/agents/data_agent.py
@@ -1,5 +1,6 @@
 import requests, feedparser
 from dataclasses import dataclass, field
+from bs4 import BeautifulSoup
  
 @dataclass
 class LocalSocialData:
@@ -40,11 +41,30 @@ class DataAgent:
     def fetch_finviz_news(self, ticker: str, company: str) -> list[str]:
         #query = f'{ticker} stock'
         query = f'{ticker}'
-        url   = (f'https://finviz.com/quote.ashx?t={query}'
+        url   = f'https://finviz.com/quote.ashx?t={query}'
 
         try:
-            feed = feedparser.parse(url)
-            return [entry.title for entry in feed.entries[:15]]
+
+        # 2. Fetch the page
+           response = requests.get(url, headers=headers)
+           soup = BeautifulSoup(response.text, 'html.parser')
+
+        # 3. Find the news table
+           news_table = soup.find(id='news-table')
+
+        # 4. Loop through the rows to get the headlines
+           for row in news_table.findAll('tr'):
+                a_tag = row.find('a')
+                if a_tag:
+                    headline = a_tag.text
+                    #link = a_tag['href']
+        # The time is in the adjacent cell
+                    #time_text = row.td.text.strip()
+        #print(f"{time_text} | {headline} | {link}")
+
+
+            #feed = feedparser.parse(url)
+           return [entry.title for entry in headline.entries[:15]]
         except Exception as e:
             print(f'Finviz News error: {e}')
             return []
@@ -54,13 +74,13 @@ class DataAgent:
         ##news       = self.fetch_google_news(ticker, company)
         #combined   = '\n'.join(stocktwits + news)
         news_finviz = self.fetch_finviz_news(ticker, company)
-        news = news_finviz
+        combined = news_finviz
 
 
         return LocalSocialData(
             ticker=ticker, company=company,
             #stocktwits_posts=stocktwits,
-            news_headlines=news
-            #,combined_text=combined
+            news_headlines=news,
+            combined_text=combined
             
         )
